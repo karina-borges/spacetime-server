826487e1bc628f9b151190325b2b812b8d743175
Karina <karina@cuponeria.com.br>
2023-05-17 19:47:54 -0300

    ✨ Create memories CRUD service

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  // TODO: change this to the domain of the frontend app
+  // origin: ['http://localhost:3000'],
+  origin: true,
 })
+
+app.register(memoriesRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 Server is running on port 3333')
